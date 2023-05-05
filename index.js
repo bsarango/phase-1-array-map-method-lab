83f8aba7104d034041d96eb35df0eda9ac02696d
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-05-05 13:58:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (() => {   //will destructively change tutorials since global variable is being used
+  return tutorials.map((titles) => {   //map function as an anonymous function
+    const splitTitles = titles.split(" ") //splits stirng into an array of each individual word
+    return splitTitles.map((wordOfTitle)=>{    //map out the new array to check individual word
+        //check to see if the first letter is capitalized
+        return wordOfTitle.charAt(0).toUpperCase() + wordOfTitle.slice(1) //return capitalized Word
+    }).join(" ")
+    //return splitTitles.join(" ") //convert the array of words to strings
+  })
+  //const brokenUpTitle = tutorials.split(" ") //Breaks up title string into individaul words stored in individual indexes in the stated array
+  //for(const word in brokenUpTitle){
+    //if(word.charAt(0) !== (word.charAt(0)).toUpperCase){  //Checks to see if the first letter of the word in the array for the Title is capital or not.
+      //titleCasedTitle = titleCasedTitle + " " + word.charAt(0).toUpperCase + word.slice(1);
+    //}
+    //else{
+      //titleCasedTitle = titleCasedTitle + " " + word;
+    //}
+  //}
+  //return tutorials;
+})
+  //return tutorials // This should return a new array, not tutorials
+
+
